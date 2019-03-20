7502043012b63fba2666a5780096283675e30365
root <root@localhost.localdomain>
2019-03-20 10:40:48 +0200

    onsubmit event to check whether user entered valid entry added

--- UI/js/login.js
+++ UI/js/login.js
@@ -22,4 +22,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const checkPass = pass => {
         return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(pass);
     };
+
+    document.querySelector("#login").onsubmit = () => {
+        if(!emailIsValie(document.querySelector("#email").value)) {
+            document.querySelector("#email").style.borderBottom = "2px solid red";
+            document.querySelector("#email").focus();
+            return false;
+        } else if(!checkPass(document.querySelector("#password").value)) {
+            document.querySelector("#password").style.borderBottom = "2px solid red";
+            document.querySelector("#password").focus();
+            return false;
+        } else {
+            return true;
+        }
+    };
 });
