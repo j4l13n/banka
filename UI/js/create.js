1be15375ed3d673e189713d2eae26b5def3e2266
root <root@localhost.localdomain>
2019-03-21 14:31:46 +0200

    create account page is done

--- UI/js/create.js
+++ UI/js/create.js
@@ -1,14 +1,46 @@
-document.querySelector("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#accountNumber").onkeyup = () => {
         if(checkAccountNumber(document.querySelector("#accountNumber").value)) {
-            document.querySelector("#accountNumber").style.borderBottom = "2px solid green";
+            document.querySelector(".form").style.borderBottom = "2px solid green";
         } else {
-            document.querySelector("#accountNumber").style.borderBottom = "2px solid red";
+            document.querySelector(".form").style.borderBottom = "2px solid red";
         }
+    };
+
+    document.querySelector("#type").onkeyup = () => {
+        if(checkType(document.querySelector("#account").selector())) {
+            document.querySelector("#type").style.borderBottom = "2px solid green";
+        } else {
+            document.querySelector("#type").style.borderBottom = "2px solid red";
+        }
+    };
+
+    document.querySelector("#balance").onkeyup = () => {
+        if(checkBalance(document.querySelector("#balance").value)) {
+            document.querySelector("#balance").style.borderBottom = "2px solid green";
+        } else {
+            document.querySelector("#balance").style.borderBottom = "2px solid red";
+        }
+    };
+
+    const checkType = type => {
+        if(type.equals("Savings") || type.equals("Current")) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    const checkBalance = balance => {
+        return /^\d*$/.test(balance);
     };
 
 
     const checkAccountNumber = number => {
-        return /^\d*$/.test(number);
+        if(/^\d*$/.test(number) && number.length === 10) {
+            return true;
+        } else {
+            return false;
+        }
     };
 });
