87e021f7c2761d69b46155b876d0833fe3413d08
root <root@localhost.localdomain>
2019-04-12 22:55:32 +0200

    [ft #165199568] Add check authorization for protected routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -24,19 +24,19 @@ const checkUser = (req, res, next) => {
 };
 
 
-router.get("/api/v1/users", checkUser, userController.getAllUsers);
-router.get("/api/v1/users/:id", userController.getUser);
+router.get("/api/v1/users", userController.getAllUsers);
+router.get("/api/v1/users/:id", checkUser, userController.getUser);
 router.post("/api/v1/auth/signup", userController.createUser);
 router.post("/api/v1/auth/signin", authController.signin);
-router.delete("/api/v1/users/:id", userController.deleteUser);
-router.get("/api/v1/accounts", accountController.getAllAccounts);
-router.post("/api/v1/accounts", accountController.createAccount);
-router.get("/api/v1/accounts/:id", accountController.getAccount);
+router.delete("/api/v1/users/:id", checkUser, userController.deleteUser);
+router.get("/api/v1/accounts", checkUser, accountController.getAllAccounts);
+router.post("/api/v1/accounts", checkUser, accountController.createAccount);
+router.get("/api/v1/accounts/:id", checkUser, accountController.getAccount);
 router.delete("/api/v1/accounts/:id", accountController.deleteAccount);
-router.patch("/api/v1/accounts/:id", accountController.activateAccount);
-router.get("/api/v1/transactions", transactionController.getAll);
-router.post("/api/v1/transactions/:acc/debit", transactionController.debitAccount);
-router.post("/api/v1/transactions/:acc/credit", transactionController.creditAccount);
+router.patch("/api/v1/accounts/:id", checkUser, accountController.activateAccount);
+router.get("/api/v1/transactions", checkUser, transactionController.getAll);
+router.post("/api/v1/transactions/:acc/debit", checkUser, transactionController.debitAccount);
+router.post("/api/v1/transactions/:acc/credit", checkUser, transactionController.creditAccount);
 router.get("/api/v1/signout", authController.signout);
 
 export default router;
