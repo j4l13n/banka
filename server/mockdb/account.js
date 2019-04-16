7d692eef02893f11cd1071de86c5f47a9c9cf700
root <root@localhost.localdomain>
2019-04-16 17:29:45 +0200

    [bg #165385661] Fix transaction endpoint tests

--- server/mockdb/account.js
+++ server/mockdb/account.js
@@ -2,7 +2,16 @@ const account = [
     {
         id : 1,
         accountNumber: 1233,
-        createOn: Date.now,
+        createOn: Date.now(),
+        owner: 1,
+        type: "Current",
+        status: "active",
+        balance: 500000.0
+    },
+    {
+        id : 2,
+        accountNumber: 1244,
+        createOn: Date.now(),
         owner: 1,
         type: "Savings",
         status: "active",
--- server/tests/transaction.test.js
+++ server/tests/transaction.test.js
@@ -25,6 +25,7 @@ describe("Transactions", () => {
             let acc = "1234";
             chai.request(app)
                 .post(`/api/v1/transactions/${acc}/debit`)
+                .send({ cashier: 1, amount: 5000 })
                 .end((err, res) => {
                     res.should.have.status(404);
                 done();
@@ -32,11 +33,102 @@ describe("Transactions", () => {
         });
 
         it("it should credit for a specific user account", done => {
-            let acc = "1233";
+            let acc = "1244";
             chai.request(app)
                 .post(`/api/v1/transactions/${acc}/credit`)
+                .send({ cashier: 1, amount: 5000 })
                 .end((err, res) => {
                     res.should.have.status(200);
+                done();
+            });
+        });
+
+        it("it should return an error when amount not specified", done => {
+            let acc = "1244";
+            chai.request(app)
+                .post(`/api/v1/transactions/${acc}/credit`)
+                .send({ cashier: 1 })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('error');
+                done();
+            });
+        });
+
+        it("it should return an error when cashier  not specified", done => {
+            let acc = "1244";
+            chai.request(app)
+                .post(`/api/v1/transactions/${acc}/credit`)
+                .send({ amount: 50000 })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('error');
+                done();
+            });
+        });
+
+        it("it should return an error when account not found", done => {
+            let acc = "1255";
+            chai.request(app)
+                .post(`/api/v1/transactions/${acc}/credit`)
+                .send({ cashier: 1, amount: 50000 })
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.an('object');
+                done();
+            });
+        });
+
+        it("it should return an error when debiting large amount", done => {
+            let acc = "1244";
+            chai.request(app)
+                .post(`/api/v1/transactions/${acc}/debit`)
+                .send({ cashier: 1, amount: 6000000 })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('error');
+                done();
+            });
+        });
+
+        it("it should debit an amount to a specific account", done => {
+            let acc = "1244";
+            chai.request(app)
+                .post(`/api/v1/transactions/${acc}/debit`)
+                .send({ cashier: 1, amount: 50000 })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('data');
+                done();
+            });
+        });
+
+        it("it should return when amount not specified", done => {
+            let acc = "1244";
+            chai.request(app)
+                .post(`/api/v1/transactions/${acc}/debit`)
+                .send({ cashier: 1 })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('error');
+                done();
+            });
+        });
+
+        it("it should return when cashier not specified", done => {
+            let acc = "1244";
+            chai.request(app)
+                .post(`/api/v1/transactions/${acc}/debit`)
+                .send({ amount: 50000 })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an('object');
+                    res.body.should.have.property('error');
                 done();
             });
         });
