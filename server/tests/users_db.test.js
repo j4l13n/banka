786c45137dcb1fd4c02b8752f84451c5619a6dd3
root <root@localhost.localdomain>
2019-04-25 16:50:36 +0200

    [ft #165566172] Add user middleware validations

--- server/tests/users_db.test.js
+++ server/tests/users_db.test.js
@@ -378,7 +378,7 @@ describe("Test all users routes", () => {
 
         it("should return admin already exist", done => {
             const user = {
-                email: "julien@gmail.com",
+                email: "admin@gmail.com",
                 firstname: "hirwa",
                 lastname: "julien",
                 password: "Regedit56",
@@ -393,12 +393,12 @@ describe("Test all users routes", () => {
                 });
         });
 
-        it("should create new user", done => {
+        it("should create normal user", done => {
             const user = {
                 email: "julien@gmail.com",
                 firstname: "hirwa",
                 lastname: "julien",
-                password: "Regedit56",
+                password: "Regedit56"
             };
             chai.request(app)
             .post(`${baseUrl}auth/signup`)
@@ -409,18 +409,33 @@ describe("Test all users routes", () => {
             });
         });
 
-        it("should return user already exist", done => {
+        it("should create new admin", done => {
             const user = {
                 email: "julien@gmail.com",
                 firstname: "hirwa",
                 lastname: "julien",
                 password: "Regedit56",
+                type: "cashier"
             };
             chai.request(app)
-                .post(`${baseUrl}auth/signup`)
+            .post(`${baseUrl}auth/admin`)
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(201);
+                done();
+            });
+        });
+
+        it("it should login a user", done => {
+            const user = {
+                email: "julien@gmail.com",
+                password: "Regedit56"
+            };
+            chai.request(app)
+                .post(`${baseUrl}auth/signin`)
                 .send(user)
                 .end((err, res) => {
-                    res.should.have.status(400);
+                    res.should.have.status(200);
                     done();
                 });
         });
