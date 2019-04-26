0bdca9eb0518f9b623aba15569b2787002b84239
root <root@localhost.localdomain>
2019-04-26 11:20:59 +0200

    [ft #165566172] fix travis error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import router from './routes/routes';
 import config from './config/config';
-import userController from './controllers/users';
+import swaggerUi from 'swagger-ui-express';
 import dotenv from 'dotenv';
 
 dotenv.config();
